33f25bb2ed39b1770610f77e0721935c796395fb
ZornStolz <juan.vera1@correo.icesi.edu.co>
2021-11-18 10:19:47 -0500

    methods for crud of user done. next step, link them to the data on mongo

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 //dummy user form db
-let user = {
+let user1 = {
   "id":"15563469",
   "idtype":"cc",
   "password":"12345",
@@ -13,25 +13,66 @@ let user = {
   "notas":[]
 };
 
+//dummy user form db
+let user2 = {
+  "id":"456397546",
+  "idtype":"cc",
+  "password":"1234",
+  "name":"paco",
+  "surname":"beltran",
+  "photo":"",
+  "active":false,
+  "notas":[
+    {"titulo":"patata 101","comentario":"fldsmdfr","id":"462136"},
+    {"titulo":"viajando ando","comentario":"ta chevere el viaje, doña marina hizo un corrientaso 1a","id":"69752"}
+  ]
+}
+
+let users = [user1, user2]
+
+/* GET: get all users*/
+router.get('/', function(req, res, next) {
+
+  let lista = "{["
+
+  users.forEach(user => {
+    lista += 
+      "{" + 
+        "id:" + user.id +","+ 
+        "password:" + user.password +","+ 
+        "name:"+ user.name +","+ 
+        "surname:" + user.surname
+     + "},"
+  });
+  lista += "]}"
+  
+  res.send(lista);
+});
+
 /* GET: get an user by id */
 router.get('/:id', function(req, res, next) {
-  if (req.params.id == user.id){
-    res.send(user);
+  var foundUser = user1
+  if (foundUser != null){
+    res.send(foundUser);
   }
 });
 
-/* POST: add a new user by id */
-router.get('/:id', function(req, res, next) {
-  if (req.params.id == user.id){
-    res.send(user);
-  }
+/* POST: add a new user*/
+router.post('/add', function(req, res, next) {
+  res.send(req.body);
 });
 
-/* GET an user by id */
-router.get('/:id', function(req, res, next) {
-  if (req.params.id == user.id){
-    res.send(user);
-  }
+/* PUT: update an user by id*/
+router.put('/update/:id', function(req, res, next) {
+  //find id, replace on data base
+  res.send(req.body);
 });
+
+/* DELETE: deletes an user by id*/
+router.delete('/delete/:id', function(req, res, next) {
+  //find id, delete on data base
+  res.send("the user with the id " + req.params.id + " was deleted succesfully");
+});
+
 
 module.exports = router;
